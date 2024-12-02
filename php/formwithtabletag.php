670c6adee410d6c0fd6c452cbed0b5ddf5fb1daf
sameersheikh01 <shaikhsameeryunus@gmail.com>
2024-12-02 23:25:08 +0530

    add

---
+++ php/formwithtabletag.php
@@ -0,0 +1,72 @@
+<html>
+<head>
+<title>Registration Form</title>
+<style>
+  table{
+       width:100%;
+     }
+     table,th,td{
+        border: 1px solid black;
+        border-collapse:collapse;
+     }
+     th,td{
+      padding: 10px;
+      text-align: left;
+      }
+   </style>
+  </head>
+<body>
+<h2>Registration Form</h2>
+
+<table>
+<tr>
+ <td><label for="name">Name:</label></td>
+ <td><input type="text" id="name" name="name"></td>
+</tr>
+<tr>
+<td><label for="email">Email:</label></td>
+<td><input type="email" id="email" name="email"></td>
+</tr>
+<tr>
+<td><label for="password">Password:</label></td>
+<td><input type="password" id="password" name="password"></td>
+</tr>
+<tr>
+<td><label>Gender:</label></td>
+<td>
+<input type="radio" id="male" name="gender" value="male">
+<label for="male">Male</label>
+<input type="radio" id="female" name="gender" Value="female">
+<label for="female">Female</label>
+<input type="radio" id="other" name="gender" value="other">
+<label for="other">Other</label>
+</td>
+</tr>
+<tr>
+<td><label for="terms">Agree to Terms and Conditions:</label></td>
+<td><input type="checkbox" id=terms" name="terms" required></td>
+</tr>
+<tr>
+<td><label for="country">Country:</label></td>
+<td>
+<select id="country" name="country">
+<option value="Usa">USA</option>
+<option value="Canada">Canada</option>
+<option value="Uk">UK</option>
+<option value="Australia">Australia</option>
+<option value="Other">Other</option>
+</select>
+</td>
+</tr>
+<tr>
+<td><label for="comments">Additional Comments:</label></td>
+<td><textarea id="comments" name="comments" rows="4" cols="30"></textarea></td>
+</tr>
+<tr>
+<td></td>
+<td><input type="submit" value="submit"></td>
+</tr>
+</table>
+</form>
+</body>
+</html>
